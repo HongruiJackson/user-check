89b86d7710b08f03e638180b9411802316f37883
JacksonZHR <1024060689@qq.com>
2025-01-24 13:02:33 +0800

    feat: 封装生成hs256Jwt的方法

--- src/main/java/com/hruiworks/usercheck/util/JwtUtils.java
+++ src/main/java/com/hruiworks/usercheck/util/JwtUtils.java
@@ -3,16 +3,46 @@ package com.hruiworks.usercheck.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 import java.util.Map;
 
+import static java.time.temporal.ChronoUnit.*;
+
+/**
+ * @author JacksonZHR
+ */
 public class JwtUtils {
 
-    public static String generateJWT(Map<String, Object> claims) {
+    public static String generateHs256Jwt(Map<String, Object> claims, long time, TemporalUnit unit) {
+
+        long millis = 0L;
+        if (unit instanceof ChronoUnit chronoUnit) {
+            millis = switch (chronoUnit) {
+                case NANOS -> time / MILLIS.getDuration().toNanos();
+                case MICROS -> time / 1000;
+                case MILLIS -> time;
+                case SECONDS -> time * SECONDS.getDuration().toMillis();
+                case MINUTES -> time * MINUTES.getDuration().toMillis();
+                case HOURS -> time * HOURS.getDuration().toMillis();
+                case HALF_DAYS -> time * DAYS.getDuration().toMillis() / 2;
+                case DAYS -> time * DAYS.getDuration().toMillis();
+                case MONTHS -> time * MONTHS.getDuration().toMillis();
+                case YEARS -> time * YEARS.getDuration().toMillis();
+                case DECADES -> time * DECADES.getDuration().toMillis();
+                case CENTURIES -> time * CENTURIES.getDuration().toMillis();
+                case MILLENNIA -> time * MILLENNIA.getDuration().toMillis();
+                case ERAS -> time * ERAS.getDuration().toMillis();
+                case FOREVER -> time * FOREVER.getDuration().toMillis();
+                default -> millis;
+            };
+        }
+
         return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS256, "duwjdiojnxc")
-                .setClaims(claims)
-                .setExpiration(new Date(System.currentTimeMillis() + 172800000L))
+                .signWith(Jwts.SIG.HS256.key().build())
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis() + millis))
                 .compact();
     }
 }
